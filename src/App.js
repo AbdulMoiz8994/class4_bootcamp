fcf3114da0d1c5cf0d5a68f571dc8a2e3980d567
AbdulMoiz8994 <63612568+AbdulMoiz8994@users.noreply.github.com>
2020-10-07 11:00:34 +0500

    add these changes in react class 4 in three filess app.css app.js message

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import React, {useState} from 'react';
 import './App.css';
-
+import {Message} from './message.js';
 function App() {
 
-  let [count, setCount] = useState(1);
+  let [count, setCount] = useState(0);
+  let [isMorning, isNight]=useState()
   return (
-    <div className="App">
-    <h2>Value of counter variable is : {count}</h2>
+    <div className={`box ${isMorning? "dayLight" : ""}`}>
+      <h1>Day Time= {isMorning? 'Morning' : 'Night'}</h1>
+
+      <Message counter={count}/>
     <br/>
     <button onClick={
       ()=>setCount(++count)
     }>
      Update counter
     </button>
+    <button onClick={()=>isNight(!isMorning)}>
+    Update Day
+    </button>
     </div>
-  );
+  )
 }
+// let [isMorning, isNight]=useState(true)
+
 // setCount(count++)
 // setCount(count+1)
 // setCount(2)
---
+++ src/message.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export  function Message (props){
+     return(
+    <h2>Volume of counter variable is:{props.counter}</h2>      
+
+
+     )
+}
